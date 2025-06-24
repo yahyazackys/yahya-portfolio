6843bd8a186b17efd3f2050b27f7b4483d269211
yahyazackys <yahyazacky123456@gmail.com>
2025-06-25 03:26:10 +0700

    new config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
   // output: 'export',
 };
 
